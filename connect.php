03a09e0bf121ff37716499a6222332606e291e75
Bhodikorn <112610961+Bhodikorn@users.noreply.github.com>
2023-06-06 19:39:03 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "bho.mysql.database.azure.com";
+$username = "bhodi";
+$password = "Treebangbang*007";
 $dbname = "pos_system";
 
 // Create connection
